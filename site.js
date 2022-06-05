c8c14c08d2fa05c9304dfef9130b9c8ecf530b76
Ashley Mays <ashleymays091@gmail.com>
2022-06-04 23:47:12 -0700

    Added javascript file to show/hide header and made some changes to the design

---
+++ site.js
@@ -0,0 +1,32 @@
+// when the user is scrolling, add a small bottom border to the header
+// if window.scrollTop is not 0, then add a small bottom border of color #6C7A89 to the header element
+
+// document.addEventListener("scroll", function() {
+//     let header = document.querySelector("header");   
+//     if (window.scrollTop != 0) {
+//         header.style.borderBottomColor = "#6C7A89";
+//     }
+//     else {
+//         header.style.borderBottomColor = "transparent";
+//     }
+// });
+
+
+let lastScrollTop, currentScrollTop = 0;
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        lastScrollTop = currentScrollTop;
+        currentScrollTop = window.scrollY;
+        
+        // user has scrolled up -> show header;
+        if (currentScrollTop <= lastScrollTop) {
+            $("header").slideDown();
+        }
+
+        // user has scrolled down -> hide header
+        else {
+            $("header").slideUp();
+        }
+    })
+})
