b525f91ef070c42332378be2231695317f9a87b0
Ashley Mays <ashleymays091@gmail.com>
2023-01-29 16:41:00 -0800

    Change Navbar to Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
-            </Routes>
-        </Router>
-    )
+    return;
 }
 
 export default App;
