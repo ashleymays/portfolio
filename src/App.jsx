25f01a11368565026e40123537cb4b103905b855
Ashley Mays <ashleymays091@gmail.com>
2023-01-29 16:39:35 -0800

    Add navbar directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-export default function App() {
-    return;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+            </Routes>
+        </Router>
+    )
 }
+
+export default App;
