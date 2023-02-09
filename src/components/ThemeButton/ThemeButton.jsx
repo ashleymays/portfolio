0d9b1a7c641508b769d0cab301281276046df999
Ashley Mays <ashleymays091@gmail.com>
2023-02-08 20:06:36 -0800

    Fix night mode functionality to persist between pages

--- src/components/ThemeButton/ThemeButton.jsx
+++ src/components/ThemeButton/ThemeButton.jsx
@@ -1,28 +1,39 @@
+// Credit for state persistence: https://felixgerschau.com/react-localstorage/
+
 import { useEffect, useState } from "react";
 import { HiOutlineSun, HiOutlineMoon } from "react-icons/hi";
 import "./ThemeButton.scss";
 
 function ThemeButton() {
-  const [theme, setTheme] = useState("Day");
+  const [isNightMode, setIsNightMode] = useState(localStorage.getItem("theme") === "Night");
 
-  const changeTheme = () => (theme === "Day" ? setTheme("Night") : setTheme("Day"));
+  const updateBackgroundColor = () => (isNightMode ? "#101010" : "#f2f2f2");
 
-  const themeIcon = theme === "Day" ? <HiOutlineSun /> : <HiOutlineMoon />;
+  const updateColor = () => (isNightMode ? "#d5d5d5" : "#121212");
 
-  useEffect(() => {
-    const updateBackgroundColor = () => (theme === "Day" ? "#f2f2f2" : "#101010");
-
-    const updateColor = () => (theme === "Day" ? "#121212" : "#d5d5d5");
-
+  const changeTheme = () => {
     const body = document.querySelector("body");
     body.style.background = updateBackgroundColor();
     body.style.color = updateColor();
-  }, [theme]);
+  };
+
+  const themeIcon = isNightMode ? <HiOutlineMoon /> : <HiOutlineSun />;
+
+  useEffect(() => {
+    changeTheme();
+    const theme = isNightMode ? "Night" : "Day";
+    localStorage.setItem("theme", theme);
+  }, [isNightMode]);
 
   return (
-    <button type="button" name="theme-button" className="flex-row flex-al-center" onClick={changeTheme}>
+    <button
+      type="button"
+      name="theme-button"
+      className="flex-row flex-al-center"
+      onClick={() => setIsNightMode(!isNightMode)}
+    >
       {themeIcon}
-      <div id="theme-title">{theme}</div>
+      <div id="theme-title">{isNightMode ? "Night" : "Day"}</div>
     </button>
   );
 }
