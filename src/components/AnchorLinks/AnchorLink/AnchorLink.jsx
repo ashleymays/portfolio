6f033b37e983bf803726c7e33ac79e059e27a4fd
Ashley Mays <ashleymays091@gmail.com>
2023-02-12 00:53:50 -0800

    Remove header animation and change contact page animation

--- src/components/AnchorLinks/AnchorLink/AnchorLink.jsx
+++ src/components/AnchorLinks/AnchorLink/AnchorLink.jsx
@@ -1,12 +1,10 @@
-import useHeadingAnimation from "../../../hooks/useHeadingAnimation";
 import "./AnchorLink.scss";
 
 function AnchorLink(props) {
-  const title = useHeadingAnimation(props.title);
   return (
     <a href={props.href} target="_blank" rel="noreferrer">
       <h2 className="flex-row flex-align-center anchorlink">
-        {title}
+        {props.title}
         <div className="anchorlink__underline" />
       </h2>
     </a>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,8 @@
-import { useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import anime from "animejs";
 import ThemeButton from "../ThemeButton/ThemeButton";
 import "./Header.scss";
 
 function Header() {
-  useEffect(() => {
-    anime
-      .timeline({
-        targets: "header *",
-        duration: 1,
-        opacity: 1,
-      })
-      .add({
-        duration: 900,
-        rotateX: [180, 0],
-        easing: "easeOutExpo",
-      });
-  }, []);
-
   return (
     <header className="flex-row flex-align-center flex-justify-space-btwn width-100">
       <NavLink to="/">Ashley Mays</NavLink>
