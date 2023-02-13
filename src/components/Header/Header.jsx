19ab5869783f2dd0ce8135d5326651d597042add
Ashley Mays <ashleymays091@gmail.com>
2023-02-12 19:19:19 -0800

    Edit header design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,6 @@ function Header() {
   return (
     <header className="flex-row flex-align-center flex-justify-space-btwn width-100">
       <NavLink to="/">Ashley Mays</NavLink>
-      <div /> {/* Spacing buffer */}
       <nav className="flex-row flex-align-center flex-justify-space-btwn">
         <NavLink to="/">Work</NavLink>
         <NavLink to="/about">About</NavLink>
