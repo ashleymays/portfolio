784b7969af830b031d8d8a67d5c5c7f4483d8074
Ashley Mays <ashleymays091@gmail.com>
2023-03-10 17:26:47 -0800

    Add design inspirations to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,15 @@ function Footer() {
     <>
       <Divider />
       <footer className="flex-col flex-align-end flex-justify-center">
-        Developed by Ashley Mays. Design inspired by Person 1, Person 2, and Person 3.
+        <span>
+          Developed by Ashley Mays. Design inspired by the websites of:{" "}
+          <span className="anchor-link-container">
+            <a href="https://www.josesentis.com/">Jose Sentis</a>, <a href="https://www.todordimov.com/">Todor Dimov</a>
+            , <a href="https://www.connormurphy.design/">Connor Murphy</a>,{" "}
+            <a href="https://www.dejan.works/">Dejan Markovic</a>,{" "}
+            <a href="https://www.olivier-guilleux.com/">Olivier Guilleux</a>.
+          </span>
+        </span>
       </footer>
     </>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.scss";
 
+// eslint-disable-next-line no-undef
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
