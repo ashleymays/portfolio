a30e66e05a8616cfc6b799a4fce4ab5280e06229
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2023-10-22 23:34:47 +0000

    Add ContactLink component inside Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,26 +1,30 @@
+function ContactLink({ href, children }) {
+    return (
+        <a className="contact-link" target="_blank" rel="noreferrer" href={href}>
+            {children}
+        </a>
+    );
+}
+
 function Contact() {
     return (
         <section className="contact">
             <div className="line" />
             <p>
                 If you want to get in touch with me, you can email me at{' '}
-                <a href="mailto:ashleymays091@gmail.com">ashleymays091@gmail.com</a>. Or you can
-                check out my <a href="https://github.com/ashleymays">Github</a> to see what I&apos;m
-                working on. Either way, thanks for reading and I&apos;ll (maybe) talk to you soon.
+                <a className="contact-link" href="mailto:ashleymays091@gmail.com">
+                    ashleymays091@gmail.com
+                </a>
+                . Or you can check out my{' '}
+                <ContactLink href="https://github.com/ashleymays">Github</ContactLink> to see what
+                I&apos;m working on. Either way, thanks for reading and I&apos;ll (maybe) talk to
+                you soon.
             </p>
             <p>
                 Design inspired by the websites of{' '}
-                <a target="_blank" rel="noreferrer" href="https://slrncl.com">
-                    Nicolas Solerieu
-                </a>
-                ,{' '}
-                <a target="_blank" rel="noreferrer" href="https://manuelmoreale.com">
-                    Manuel Moreale
-                </a>
-                , and{' '}
-                <a target="_blank" rel="noreferrer" href="https://brianlovin.com">
-                    Brian Lovin.
-                </a>
+                <ContactLink href="https://slrncl.com">Nicolas Solerieu</ContactLink>,{' '}
+                <ContactLink href="https://manuelmoreale.com">Manuel Moreale</ContactLink>, and{' '}
+                <ContactLink href="https://brianlovin.com">Brian Lovin.</ContactLink>
             </p>
         </section>
     );
